0b053dc2bd931d8af5b49545f0147e9737a85284
barroqt <barroqt@gmail.com>
2026-06-04 23:09:22 +0200

    parse header to return correlation_id

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self, Read, Write};
 use std::net::TcpListener;
 
 fn main() -> Result<(), io::Error> {
@@ -6,7 +6,16 @@ fn main() -> Result<(), io::Error> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {}
+            Ok(mut stream) => {
+                let mut header = [0u8; 12];
+                stream.read_exact(&mut header)?;
+
+                let message_size_bytes = &header[0..4];
+                let correlation_id_bytes = &header[8..12];
+
+                let response = [message_size_bytes, correlation_id_bytes].concat();
+                stream.write_all(&response)?;
+            }
             Err(e) => eprintln!("Connection failed: {e}"),
         }
     }
