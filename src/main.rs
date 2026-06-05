6e2a5e6e8b589ee1bd5bcc5912b8dcfeee8d4155
barroqt <barroqt@gmail.com>
2026-06-05 15:51:39 +0200

    listing partitions

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,14 @@ fn handle_connection(mut stream: impl Read + Write) -> Result<(), io::Error> {
         body.extend_from_slice(&error_code.to_be_bytes());
 
         if error_code == 0 {
-            body.push(0x02);
+            body.push(0x03);
             body.extend_from_slice(&18i16.to_be_bytes());
             body.extend_from_slice(&0i16.to_be_bytes());
             body.extend_from_slice(&4i16.to_be_bytes());
+            body.push(0x00);
+            body.extend_from_slice(&75i16.to_be_bytes());
+            body.extend_from_slice(&0i16.to_be_bytes());
+            body.extend_from_slice(&0i16.to_be_bytes());
             body.push(0x00);
         } else {
             body.push(0x01);
